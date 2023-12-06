e09663ee617f2a6b02384833448603fedbe7a6ed
Austen Gabrielpillai <a.gabrielpillai@gmail.com>
2023-12-06 10:22:46 -0500

    development update

--- simulation.py
+++ simulation.py
@@ -6,7 +6,8 @@ from tqdm import tqdm
 class leaf():
     def __init__(self, bbox=np.array([[-1, 1],[-1, 1]]), depth=1):
         # if the leaf has a particle ... 
-        self.particle = None 
+        self.mass = None
+        self.com = None
         # center position of quadrant 
         self.center = np.array([bbox[0].sum() / 2, bbox[1].sum() / 2])
         # length of bounding box, same for x- and y- axes 
@@ -21,8 +22,8 @@ class QuadTree():
         self.center = oleaf.center
         self.length = oleaf.length
         self.depth = oleaf.depth
-        self.mass = 0
-        self.com = 0 
+        self.mass = oleaf.mass 
+        self.com = oleaf.com 
         
         # initialize leaf nodes ... 
         self.ll = leaf(depth = self.depth + 1, bbox=np.array([[oleaf.center[0] - oleaf.length / 2, oleaf.center[0]], 
@@ -33,32 +34,9 @@ class QuadTree():
                                  [oleaf.center[1], oleaf.center[1] + oleaf.length / 2]]))
         self.lr = leaf(depth = self.depth + 1, bbox=np.array([[oleaf.center[0], oleaf.center[0] + oleaf.length / 2], 
                                  [oleaf.center[1] - oleaf.length / 2, oleaf.center[1]]]))
-                
-
-def plotter(node, c, alpha=0.1):
-    plt.plot(
-    [node.center[0] + node.length / 2, node.center[0] - node.length / 2, node.center[0] - node.length / 2, 
-     node.center[0] + node.length / 2, node.center[0] + node.length / 2],
-    [node.center[1] + node.length / 2, node.center[1] + node.length / 2, node.center[1] - node.length / 2, 
-     node.center[1] - node.length / 2, node.center[1] + node.length / 2], 
-    alpha=alpha, c=c)
-    
-    return 0 
-
-
-def drawTree(node): 
-    if isinstance(node, leaf):
-        if node.particle is not None: 
-            plotter(node, 'c', 0.2)
-            return 0 
-    else: 
-        plotter(node, 'white', 0.05)
-        _ = [drawTree(n) for n in [node.ul, node.ur, node.ll, node.lr]]
-
+   
         
 def quadAssign(p, node): 
-    # grab the angle to assign to a quadrant (in degrees). arctan2 returns [0, 180] and [-180, 0]
-    # need to take the angle of the particle relative to the evaluated node
     theta = np.arctan2(p[1] - node.center[1], p[0] - node.center[0]) * 180 / np.pi
     if theta < 90 and theta > 0: 
         node.ur = assignParticle(p, node.ur)
@@ -73,75 +51,104 @@ def quadAssign(p, node):
         
 def assignParticle(p, node):
     # if a leaf has no particle, assign particle to leaf 
-    if isinstance(node, leaf) and node.particle is None:
-        node.particle = p 
+    if isinstance(node, leaf) and node.com is None:
+        node.com = p
+        node.mass = 1
     # if a leaf has a particle, make it a tree and re-assign particles
-    elif isinstance(node, leaf) and node.particle is not None: 
-        p_old = node.particle 
+    elif isinstance(node, leaf) and node.com is not None: 
+        p_old = node.com 
         node = QuadTree(node)
         node = assignParticle(p, node)  
         node = assignParticle(p_old, node)
+        node.com = (p_old + p) / 2
+        node.mass = 2
     # if tree, find out where in the tree the particle should live 
     else: 
-        node.mass = node.mass + 1 
         node = quadAssign(p, node)
+        node.com = (node.com * node.mass + p) / (node.mass + 1)
+        node.mass = node.mass + 1
         
     return node
     
 
-# function holding our quantities we want to get d/dt for 
-# so we can use them in RK function 
-def f(r, t): 
-    x = r[:, 0]
-    y = r[:, 1]
-    xdot = r[:, 2]
-    ydot = r[:, 3]
+def solve_acc(p, node, epsilon=0.0, G = 1e-3, m_scale=1, r_scale=1): 
+    # do psudo periodic boundary conditions here 
+    return G * m_scale * node.mass * (node.com - p + epsilon) / sum((node.com - p + epsilon)**2)**(3 / 2)
+
+
+def f_multipole(p, node, theta=0.0):  
+    # check to see if particle is looking at itself,return 0 
+    if node.mass is not None and sum((node.com - p)**2)**0.5 < 1e-10:
+        return np.zeros(2) 
     
-    a = xdot
-    b = ydot
-    da = np.ones(len(x)) * 0
-    db = np.ones(len(x)) * 0
+    # if a leaf node and has a particle, directly solve for gravity 
+    if isinstance(node, leaf) and node.mass is not None:
+        return solve_acc(p, node)
+    # if a leaf node and doesn't have a particle, return 0
+    elif isinstance(node, leaf) and node.mass is None:
+        return np.zeros(2)
+    
+    # if a quadtree that satisfies multipole condition, solve for gravity
+    if isinstance(node, QuadTree) and node.length / sum((node.com - p)**2)**0.5 <= theta: 
+        return solve_acc(p, node)
+    # else if quadtree and hasn't satisfied, recursively search the tree's children 
+    elif isinstance(node, QuadTree) and node.length / sum((node.com - p)**2)**0.5 > theta: 
+        return sum(np.array([f_multipole(p, ch, theta) for ch in [node.ll, node.ur, node.lr, node.ul]]))
+        
 
-    return np.array([a, b, da, db], float).T
+def leapfrog(r, t_start=0, t_end=10, N=1e4, L=2):
+    dt = 0.01 # (t_end - t_start)/N
 
-
-def RK4(r, t_start=0, t_end=10, N=1e4):
-    h = 0.1 # (t_end - t_start)/N
-
-    tpoints = np.arange(t_start, t_end, h)
+    tpoints = np.arange(t_start, t_end, dt)
     xpoints = []
     trees = []
+    
+    acc = np.zeros((len(r), 2))
+    pos = r[:, 0:2]
+    vel = r[:, 2: ]
 
-    for t in tpoints:
+    for t in tqdm(tpoints):
         # have to .copy() because we are updating the same array and that causes memory issues 
-        xpoints.append(r.copy())
+        xpoints.append(np.hstack((pos, vel, acc)))
         
-        # we start with a simple leaf (root) 
+        # use current acceleration to kick velocity a half time step
+        vel = vel + acc * dt / 2 
+        # drift the position using the kicked (half time step) velocity 
+        pos = pos + vel * dt 
+        
+        # update acceleration block. construct the quadtree for force modeling 
         root = leaf()
         # which we then can immediately turn into a tree 
-        for p in r: 
-            root = assignParticle(p[0:2], root)
+        for p in pos: 
+            root = assignParticle(p, root)
+        # calculate acceleration 
+        acc = np.array([f_multipole(p, root, 0) for p in pos])
+        
+        # kick velocity a half time step using new updated acceleration  
+        vel = vel + acc * dt / 2 
+        
+        # fix positions based on boundary conditions 
+        pos[pos[:, 0] > L/2, 0] = pos[pos[:, 0] > L/2, 0] - L
+        pos[pos[:, 0] < -L/2, 0] = pos[pos[:, 0] < -L/2, 0] + L
+        pos[pos[:, 1] > L/2, 1] = pos[pos[:, 1] > L/2, 1] - L
+        pos[pos[:, 1] < -L/2, 1] = pos[pos[:, 1] < -L/2, 1] + L
+        
         trees.append(root)
-
-        k1 = h*f(r, t)
-        k2 = h*f(r + 0.5*k1, t + 0.5*h)
-        k3 = h*f(r + 0.5*k2, t + 0.5*h)
-        k4 = h*f(r + k3, t + h)
         
-        r += (k1 + 2*k2 + 2*k3 + k4) / 6
-        
-        # boundary condition fixing 
-        r[r[:, 0] > 1, 0] = r[r[:, 0] > 1, 0] - 2
-        r[r[:, 0] < -1, 0] = r[r[:, 0] < -1, 0] + 2
-        r[r[:, 1] > 1, 1] = r[r[:, 1] > 1, 1] - 2
-        r[r[:, 1] < -1, 1] = r[r[:, 1] < -1, 1] + 2
-        
-    return tpoints, np.array(xpoints), trees
+    return tpoints, np.array(xpoints), np.array(trees)
 
 
-def leapfrog():
-    return 0 
-
-if name == __main__: 
-    theta = 0.1
+if __name__ == "__main__":
+    # some constants ... 
+    L = 2
+    n = 10
+    theta = 0 
     softening = 0.01
+    
+    
+    particles = np.random.random((n, 4)) * 0.5 - 0.5 / 2 # L - L / 2 
+    particles[:, 2] = 0 # np.random.random(n)
+    particles[:, 3] = 0 # np.random.random(n) - 0.5
+    # particles = np.array([[0.15, 0, 0, 0], [-0.15, -0, 0, 0]])
+
+    tpoints, particles_list, trees = leapfrog(particles.copy())
---
+++ visualizer.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+import time 
+from natsort import natsorted
+import moviepy.video.io.ImageSequenceClip
+
+from simulation import leaf, QuadTree
+        
+def plotter(ax, node, c, alpha=0.1):
+    ax.plot(
+    [node.center[0] + node.length / 2, node.center[0] - node.length / 2, node.center[0] - node.length / 2, 
+     node.center[0] + node.length / 2, node.center[0] + node.length / 2],
+    [node.center[1] + node.length / 2, node.center[1] + node.length / 2, node.center[1] - node.length / 2, 
+     node.center[1] - node.length / 2, node.center[1] + node.length / 2], 
+    alpha=alpha, c=c)
+#     plt.scatter(node.com[0], node.com[1])
+    
+    return 0 
+
+
+def drawTree(ax, node): 
+    if isinstance(node, leaf):
+        if node.com is not None: 
+            plotter(ax, node, 'c', 0.2)
+            return 0 
+    else: 
+        plotter(ax, node, 'white', 0.05)
+        _ = [drawTree(ax, n) for n in [node.ul, node.ur, node.ll, node.lr]]
+        
+        
+
+def renderParticles(particles_list, trees, tpoints, save_dir):
+    for particles, tree, t in zip(particles_list, trees, tpoints): 
+        fig, ax = plt.subplots(1, 1, figsize=(6, 6))
+        ax.set_xlim(-1.05, 1.05)
+        ax.set_ylim(-1.05, 1.05)
+        ax.set_facecolor("black")
+        ax.set_xlabel('x [Mpc]')
+        ax.set_ylabel('y [Mpc]')
+
+        drawTree(ax, tree)
+        ax.scatter(particles[:, 0], particles[:, 1], c='white', s=0.5)
+        plt.savefig('{}/frame_{}.png'.format(save_dir, t), bbox_inches='tight', pad_inches = 0, dpi=200)
+        plt.close()
+            
+    return
+
+
+if __name__ == "__main__": 
+    tstart = time.time() 
+
+    print('[{:.2f}] Creating plot directory ...'.format(time.time() - tstart))
+    new_dir = '{}/{}'.format(os.path.abspath(os.getcwd()), 'L{}n{}'.format(L, n))
+    if not os.path.isdir(new_dir):
+        os.mkdir(os.path.join(new_dir))
+
+    print('[{:.2f}] Initializing multiprocessing ...'.format(time.time() - tstart))
+    n_cpu = mp.cpu_count()
+    pool = mp.Pool(processes=n_cpu)
+
+    mp_trees = np.array_split(trees, n_cpu)
+    mp_particles = np.array_split(particles_list, n_cpu)
+    mp_tpoints = np.array_split(np.arange(len(tpoints)), n_cpu)
+
+    print('[{:.2f}] Creating plots ...'.format(time.time() - tstart))
+    for i in range(n_cpu):
+        pool.apply_async(renderParticles, args=(mp_particles[i], mp_trees[i], mp_tpoints[i], new_dir))
+
+    pool.close()
+    pool.join()
+    print('[{:.2f}] Multiprocess concluded ...'.format(time.time() - tstart))
+
+
+    print('[{:.2f}] Creating video ...'.format(time.time() - tstart))
+
+    fps=30 #number of frames per second
+    image_files = natsorted([os.path.join(new_dir,img) for img in os.listdir(new_dir) if img.endswith(".png")], reverse=False)
+    clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
+    clip.write_videofile('test.mp4')
+
+    print('[{:.2f}] Video created ...'.format(time.time() - tstart))
+
+    _ = [os.remove(image_file) for image_file in image_files]
+    print('[{:.2f}] Directory cleaned ...'.format(time.time() - tstart))
